0f26b27f1bc1049e35ab704f4885f614785b891f
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-07-18 11:11:47 -0700

    added licenses

--- src/client/kotlin/com/cozycubes/cobbledchao/client/chao/ChaoRenderer.kt
+++ src/client/kotlin/com/cozycubes/cobbledchao/client/chao/ChaoRenderer.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2024 Cozy Cubes Studios
+ *
+ * This Source Code Form is subject to the terms of the MIT License.
+ * If a copy of the MIT was not distributed with this
+ * file, You can obtain one at https://opensource.org/licenses/MIT.
+ */
 package com.cozycubes.cobbledchao.client.chao
 
 import com.cozycubes.cobbledchao.chao.ChaoEntity
